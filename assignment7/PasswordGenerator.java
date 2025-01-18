85a84068de1e47f223458d40afd417b06592ce38
Pritanshu69 <samantaprit1924@gmail.com>
2025-01-18 21:54:45 +0530

    hue hue

--- assignment7/PasswordGenerator.java
+++ assignment7/PasswordGenerator.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class PasswordGenerator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Taking input from the user
         System.out.print("Enter your first name: ");
         String firstName = sc.nextLine();
 
@@ -17,17 +15,10 @@ public class PasswordGenerator {
         System.out.print("Enter your roll number: ");
         String rollNumber = sc.nextLine();
 
-        // Generate initials
         String initials = firstName.charAt(0) + 
-                          (middleName.isEmpty() ? "" : String.valueOf(middleName.charAt(0))) +
-                          lastName.charAt(0);
-
-        // Get last 4 digits of roll number
-        String lastFour = rollNumber.length() > 4 
-                          ? rollNumber.substring(rollNumber.length() - 4) 
-                          : rollNumber;
-
-        // Generate and display the password
-        System.out.println("Generated Password: " + initials.toUpperCase() + lastFour);
+              (middleName.isEmpty() ? "" : String.valueOf(middleName.charAt(0))) +
+              lastName.charAt(0);
+              String lastFour = rollNumber.length() > 4 ? rollNumber.substring(rollNumber.length() - 4) : rollNumber;
+              System.out.println("Generated Password: " + initials.toUpperCase() + lastFour);
     }
 }
